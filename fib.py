a8d7c80fb9cf49e3a4fb6d1a97de6fcd99a76104
Ilya Veryasov <ilyaveryasov@gmail.com>
2020-04-12 15:42:07 +0500

    mod to recursive function

--- fib.py
+++ fib.py
@@ -1,16 +1,7 @@
 def fib(n):
-    fib0 = 0
-    fib1 = 1
-    index = 1
-    if n == 0:
+    if n == 0: 
         return 0
     if n == 1:
         return 1
     else:
-        while index != n:
-            fib = fib0 + fib1
-            fib0 = fib1
-            fib1 = fib
-            index += 1
-        return fib
-print('Created by lohpidor')
+        return fib(n-1) + fib(n-2)
--- solution.py
+++ solution.py
@@ -1,3 +1,3 @@
 from fib import fib
-n = input()
-print('Fibonacci number is ' + str(fib(int(n))))
+n = int(input)
+print('Fibonacci number is' + str(fib(n)))
